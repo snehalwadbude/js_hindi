336466d5d8ca40f771d8f0dfb81bec8a0213d9e0
snehalwadbude <spwadbude@gmail.com>
2025-07-31 20:29:42 +0530

    javascript

--- 03_conversion.js
+++ 03_conversion.js
@@ -8,3 +8,36 @@ console.log(typeof Score)
 
 let valueinnumber = Number(Score)  // see during conversion hume wo jis datatype me convert karana hai use as a caoital letter me likhana hota hai
 console.log(typeof valueinnumber)
+
+console.log(valueinnumber)
+
+let empty = " "
+console.log(typeof empty);
+console.log( empty);
+
+let sam = 90
+let valueinscore = String(sam)
+console.log(typeof valueinscore);
+
+
+let parag = null
+console.log(typeof parag);
+let valueinparag = Number(parag)
+console.log(typeof valueinparag)
+console.log(valueinparag);
+
+// "33" => 33   string asani se number bnn gya but
+// "33abc "=> ye change nhi ho paya just becoz koi utpatang si chiz aa gyi haii
+//  so ye output show krega "Nan"
+// true =>1 , false = > 0
+
+
+let loggedin = 1
+let booleanloggedin = Boolean(loggedin)  //conversion of 1 in boolean
+console.log(booleanloggedin);
+
+
+
+
+
+
