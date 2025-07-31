0cbe5df2707c84f7cd90715d3857852681c6f4c8
snehalwadbude <spwadbude@gmail.com>
2025-08-01 00:04:02 +0530

    comparison

---
+++ 05_comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+console.log("2"> 1)
+console.log("02"> 1)
+console.log(null == 0);  
+console.log(null >= 0);
+
+console.log(null > 0);
+console.log(undefined ==0); // all answers false hi aaye hai bss
+console.log(undefined >=0); 
+console.log(undefined >0); 
+
+// === strict check 
+
+console.log("2"===2); // yaha pr strictly check hoga , bolega ki same datatype hai ki nahi
+
+
