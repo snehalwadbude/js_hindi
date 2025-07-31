c7aa9d1bd06ba3a179a760a291664d4a81d88fea
snehalwadbude <spwadbude@gmail.com>
2025-07-31 23:50:49 +0530

    operations

---
+++ 04_operations.js
@@ -0,0 +1,41 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+console.log(4+5);
+console.log(4-5);
+console.log(4*5);
+console.log(4/5);
+console.log(4**5); //power
+console.log(4%5); //remainder
+
+// concatination
+
+let str1 = "snehal"
+let str2 = " hello"
+let str3 = str1+str2
+console.log(str3);
+
+
+console.log("hello" + "world");
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1" + "2");
+console.log("1" + 2 +2); //122 aya hai as a pehle string ho gya wo
+console.log(1 + 2 +"2"); //32   firstly add ho gya pehle numbers the na
+
+
+console.log(+true);//OMG 1 aa gya
+console.log(+"");// 0 aa gya
+
+
+let gamecounter = 100 
+
+// gamecounter++  postfix
+++gamecounter // prefix 
+console.log(gamecounter);
+
+
+
+
+
