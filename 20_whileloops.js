8400b7daefaf369c4760d903c41fb2da20ac7fc6
snehalwadbude <spwadbude@gmail.com>
2025-08-12 01:00:59 +0530

    while loop

---
+++ 20_whileloops.js
@@ -0,0 +1,32 @@
+
+let index = 0
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+     index = index + 2
+
+}
+
+let myarray = ["flash ", "batman", "superman"]
+let arr = 0;
+while (arr <myarray.length) {
+
+    console.log(`value is ${myarray[arr]}`);
+    arr=arr+1
+    
+}
+
+//do me kam hoga then condition check hogi in while
+let score =1
+do {
+ console.log(`score is ${score}`);
+ score = score+1
+    
+} while (score<=10);
+
+
+let scoree =11 // do while loop ek baaar  to run hoga hi hoga , condition galat hui fir bhii
+do {
+ console.log(`score is ${scoree}`);
+ scoree = scoree+1
+    
+} while (scoree<=10);
